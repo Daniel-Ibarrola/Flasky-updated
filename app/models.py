0f91a35084c8dd6e7aee3343c704d05542d63f43
Daniel-Ibarrola <dis96051@gmail.com>
2023-03-31 18:24:46 -0600

    Blog posts

--- app/models.py
+++ app/models.py
@@ -18,9 +18,18 @@ def load_user(user_id):
 class Permission:
     FOLLOW = 1
     COMMENT = 2
-    WRITE = 4
+    WRITE_ARTICLES = 4
     MODERATE = 8
     ADMIN = 16
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True,
+                          default=datetime.datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
 
 class Role(db.Model):
@@ -89,6 +98,7 @@ class User(UserMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
