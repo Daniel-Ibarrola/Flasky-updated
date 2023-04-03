d49aecd7b4f8b1309ac6c205d643f8b8f8af821e
Daniel-Ibarrola <dis96051@gmail.com>
2023-04-03 17:42:14 -0600

    Posts pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import abort, flash, render_template, redirect, url_for
+from flask import abort, current_app,  flash, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -19,8 +19,13 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for(".index"))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get("page", 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
+        error_out=False
+    )
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
 
 @main.route("/user/<username>")
