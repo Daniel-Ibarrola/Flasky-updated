b9150460a96deaca373e3dc366d5f99245ca9080
Daniel-Ibarrola <dis96051@gmail.com>
2023-04-04 21:37:44 -0600

    Server side handling with markdown and bleach

--- app/models.py
+++ app/models.py
@@ -1,18 +1,15 @@
 import datetime
 import hashlib
+import bleach
 from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import URLSafeTimedSerializer as Serializer
 from itsdangerous.exc import BadSignature
+from markdown import markdown
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
 from . import login_manager
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
 class Permission:
@@ -21,15 +18,6 @@ class Permission:
     WRITE_ARTICLES = 4
     MODERATE = 8
     ADMIN = 16
-
-
-class Post(db.Model):
-    __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, index=True,
-                          default=datetime.datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
 
 class Role(db.Model):
@@ -219,4 +207,34 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 login_manager.anonymous_user = AnonymousUser
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True,
+                          default=datetime.datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = [
+            "a", "abbr", "acronym", "b", "blockquote", "code",
+            "em", "i", "li", "ol", "pre", "strong", "ul", "h1",
+            "h2", "h3", "p"
+        ]
+        target.body_html = bleach.linkify(
+            bleach.clean(markdown(value, output_format="html"),
+                         tags=allowed_tags, strip=True),
+        )
+
+
+db.event.listen(Post.body, "set", Post.on_changed_body)
